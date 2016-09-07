639bdb98f34bcc94736ea82b2747ff8a030b2f86
wangsheng666 <1028742881@qq.com>
2016-09-07 11:36:08 +0800

    用Handler在新线程中发送消息，然后在主线程中处理消息

---
+++ app/src/main/java/com/example/a123/test26/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.a123.test26;
+
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.textview);
+
+        final Handler handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                textView.setText(msg.arg1+"");
+            }
+        };
+
+
+        final Runnable myWorker = new Runnable() {
+            @Override
+            public void run() {
+                int progress = 0;
+                while(progress <= 100){
+                    Message msg = new Message();
+                    msg.arg1 = progress;
+                    handler.sendMessage(msg);
+                    progress += 1;
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Message msg = handler.obtainMessage();//同 new Message();
+                msg.arg1 = -1;
+                handler.sendMessage(msg);
+            }
+        };
+
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Thread workThread = new Thread(null, myWorker, "WorkThread");
+                workThread.start();
+
+            }
+        });
+    }
+}
